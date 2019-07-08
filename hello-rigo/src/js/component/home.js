2edf7fe780830c21516f6800bbc60b408fc38a9e
Guensie <guensie@badgelist.com>
2019-07-08 23:09:52 +0000

    Initial set up of first page (login)

--- hello-rigo/src/js/component/home.js
+++ hello-rigo/src/js/component/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -8,18 +8,25 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+				<div
+					className="jumbotron bg-info mx-auto"
+					style={{ width: "50%" }}>
+					<h1 className="display-4">Welcome to the Shoestore!</h1>
+					<p className="lead" />
+					<hr className="my-4" />
+					<a
+						className="btn btn-dark btn-lg .text-white mr-2"
+						href="#"
+						role="button">
+						<Link to="/signup">Sign Up </Link>
+					</a>
+					<a
+						className="btn btn-dark btn-lg .text-white"
+						href="#"
+						role="button">
+						Login
+					</a>
+				</div>
 			</div>
 		);
 	}
